77715af45b7c6f016ac26113d55b80054d320919
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-25 15:16:28 +0545

    refactor: convert inmem db to hashmap

--- posts/src/repository/db.rs
+++ posts/src/repository/db.rs
@@ -1,18 +1,29 @@
+use std::collections::HashMap;
+
 use crate::models::Post;
 
 #[derive(Clone, Debug, Default)]
 pub struct Db {
-    pub posts: Vec<Post>,
+    pub posts: HashMap<String, Post>,
 }
 
 impl Db {
     pub fn add_post<'a>(&mut self, post: &'a Post) -> &'a Post {
-        self.posts.push(post.clone());
+        self.posts
+            .entry(post.id().to_string())
+            .or_insert(post.clone());
 
         post
     }
 
-    pub fn find(&self) -> &[Post] {
-        &self.posts[..]
+    pub fn find_by_id(&self, id: &str) -> &Post {
+        match self.posts.get(id) {
+            Some(p) => p,
+            None => panic!("Post with id {id} not found"),
+        }
+    }
+
+    pub fn fetch_all(&self) -> Vec<&Post> {
+        self.posts.values().collect::<Vec<&Post>>()
     }
 }
