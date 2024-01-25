207001482e1a328a22d4bf9587da393c41535288
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-25 14:39:41 +0545

    feat: create constructors and getters for Post

--- posts/src/models/post.rs
+++ posts/src/models/post.rs
@@ -1,7 +1,25 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Post {
     id: String,
     title: String,
+}
+
+impl Post {
+    pub fn new(title: &str) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            title: title.to_string(),
+        }
+    }
+
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
 }
