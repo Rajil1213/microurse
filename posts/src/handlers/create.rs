cae682cdd3c4e594ffb436474a2ab82d2c4e6c05
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-27 15:35:18 +0545

    feat: implement handlers to create and fetch posts

--- posts/src/handlers/create.rs
+++ posts/src/handlers/create.rs
@@ -1,1 +1,12 @@
-pub async fn create() {}
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+
+use crate::{dtos::NewPostInput, repository::Db};
+
+pub async fn create(
+    State(db): State<Db>,
+    Json(post): Json<NewPostInput>,
+) -> Result<impl IntoResponse, ()> {
+    let created_post = db.add_post(post.title.as_str());
+
+    Ok((StatusCode::CREATED, Json(created_post.clone())))
+}
--- posts/src/handlers/fetch.rs
+++ posts/src/handlers/fetch.rs
@@ -1,1 +1,9 @@
-pub async fn fetch() {}
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+
+use crate::repository::Db;
+
+pub async fn fetch(State(db): State<Db>) -> Result<impl IntoResponse, ()> {
+    let posts = db.fetch_all();
+
+    Ok((StatusCode::OK, Json(posts)))
+}
