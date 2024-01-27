62cc794feb0ba07bc93774417e166ddc56fcb431
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-27 15:34:51 +0545

    fix: wrap hashmap in Arc<RwLock>
    To satisfy Send (and axum's requirements)

--- posts/src/repository/db.rs
+++ posts/src/repository/db.rs
@@ -1,26 +1,38 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 use crate::models::Post;
 
 #[derive(Clone, Debug, Default)]
 pub struct Db {
-    pub posts: HashMap<String, Post>,
+    pub posts: Arc<RwLock<HashMap<String, Post>>>,
 }
 
 impl Db {
-    pub fn add_post<'a>(&mut self, post: &'a Post) -> &'a Post {
+    pub fn add_post(&self, title: &str) -> Post {
+        let mut posts = self.posts.write().unwrap();
+        let new_post = Post::new(title);
+
+        posts
+            .entry(new_post.id().to_string())
+            .or_insert(new_post)
+            .clone()
+    }
+
+    pub fn find_by_id(&self, id: &str) -> Option<Post> {
+        let posts = self.posts.read().unwrap();
+
+        posts.get(id).cloned()
+    }
+
+    pub fn fetch_all(&self) -> Vec<Post> {
         self.posts
-            .entry(post.id().to_string())
-            .or_insert(post.clone());
-
-        post
-    }
-
-    pub fn find_by_id(&self, id: &str) -> Option<&Post> {
-        self.posts.get(id)
-    }
-
-    pub fn fetch_all(&self) -> Vec<&Post> {
-        self.posts.values().collect::<Vec<&Post>>()
+            .read()
+            .unwrap()
+            .values()
+            .cloned()
+            .collect::<Vec<Post>>()
     }
 }
