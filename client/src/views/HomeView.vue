b12d137843ce176db6283ac33722e3312759a868
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-31 11:58:14 +0545

    refactor: update home to use CreatePost

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import CreatePost from "@/components/posts/CreatePost.vue";
 </script>
 
 <template>
-  <main>
-    Welcome!
-  </main>
+  <header>
+    <div class="text-center bg-teal-100 p-2 mb-5">
+      <h1>Blog App</h1>
+    </div>
+  </header>
+  <CreatePost />
 </template>
