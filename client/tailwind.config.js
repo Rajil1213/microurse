dea3e4cbed65ed6bf07488b23f2d854ac043b72f
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-31 11:59:05 +0545

    feat: add tailwindcss config

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ["focus-visible"],
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [],
+};
