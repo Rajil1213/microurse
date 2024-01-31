81250dd20696429cdd14e90b84ac6517a6bc87fb
Rajil Bajracharya <imrb1213@gmail.com>
2024-01-31 11:59:47 +0545

    fix: add cors headers

--- posts/src/main.rs
+++ posts/src/main.rs
@@ -1,18 +1,25 @@
-use axum::{routing::get, Router};
+use axum::{http::Method, routing::get, Router};
 use posts::{
     handlers::{create, fetch},
     repository::Db,
 };
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 use tracing::info;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers(Any)
+        .allow_origin(Any);
+
     let app = Router::new()
         .route("/posts", get(fetch).post(create))
-        .with_state(Db::default());
+        .with_state(Db::default())
+        .layer(cors);
 
     const HOST: &str = "0.0.0.0";
     const PORT: usize = 4000;
