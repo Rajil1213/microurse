eb1069d548c9d34593a07cc849939b1dbfd2a11a
Rajil Bajracharya <imrb1213@gmail.com>
2024-02-12 14:08:13 +0545

    fix: handle all event types

--- moderation/src/models/event.rs
+++ moderation/src/models/event.rs
@@ -4,9 +4,29 @@ use uuid::Uuid;
 use crate::constants::BLACKLISTED_WORDS;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Post {
+pub struct PostEvent {
     pub id: Uuid,
     pub title: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommentCreatedEvent {
+    pub post_id: Uuid,
+    // TODO: convert to `HashMap<Uuid, Comment>`
+    pub comments: Vec<Comment>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommentModeratedEvent {
+    pub post_id: Uuid,
+    pub comment_id: Uuid,
+    pub status: CommentStatus,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommentUpdatedEvent {
+    pub post_id: Uuid,
+    pub comment: Comment,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -27,7 +47,7 @@ pub struct Comment {
 impl Comment {
     pub fn check(&self) -> CommentStatus {
         for blacklisted_word in BLACKLISTED_WORDS {
-            if self.content.contains(blacklisted_word) {
+            if self.content.to_lowercase().contains(blacklisted_word) {
                 return CommentStatus::Rejected;
             }
         }
@@ -36,22 +56,10 @@ impl Comment {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CommentCreatedEvent {
-    pub post_id: Uuid,
-    // TODO: convert to `HashMap<Uuid, Comment>`
-    pub comments: Vec<Comment>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CommentModeratedEvent {
-    pub post_id: Uuid,
-    pub comment_id: Uuid,
-    pub status: CommentStatus,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", content = "data")]
 pub enum Event {
+    PostCreated(PostEvent),
     CommentCreated(CommentCreatedEvent),
     CommentModerated(CommentModeratedEvent),
+    CommentUpdated(CommentUpdatedEvent),
 }
