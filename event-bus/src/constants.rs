fafeef4330a504ccc6787caa833383be19d1b393
Rajil Bajracharya <imrb1213@gmail.com>
2024-02-16 15:01:02 +0545

    fix: use correct hostname for query service

--- event-bus/src/constants.rs
+++ event-bus/src/constants.rs
@@ -5,7 +5,7 @@ pub const COMMENTS_PORT: usize = 4001;
 pub const COMMENTS_URL: &str = "http://comments-srv";
 
 pub const QUERIES_PORT: usize = 4004;
-pub const QUERIES_URL: &str = "http://queries-srv";
+pub const QUERIES_URL: &str = "http://query-srv";
 
 pub const MODERATION_PORT: usize = 4005;
 pub const MODERATION_URL: &str = "http://moderation-srv";
